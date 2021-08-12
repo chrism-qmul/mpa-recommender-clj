9dee275c7a4fc820ba07dc3a83f7b06f9ecb9bc9
Chris Madge <chris@madg.es>
2021-08-12 12:31:35 +0100

    added alternate import options

--- src/mpa_recommender_clj/import.clj
+++ src/mpa_recommender_clj/import.clj
@@ -12,6 +12,16 @@
    (map (fn [[_ mention_id doc_id _ annotator_id _ label]]
 	{:uuid annotator_id :itemid (str doc_id ";" mention_id) :label label}))))))
 
+(defn csv-data->map [csv-data]
+  (map zipmap (->> (first csv-data)
+                   (map keyword)
+                   repeat)
+       (rest csv-data)))
+
+(defn import-csv-basic [csv-file-path]
+  (csv-data->map (-> csv-file-path (io/reader) (csv/read-csv))))
+
+;(db/mass-insert! (import-csv-basic "ta_mpa.csv"))
 ;(take 5 )
 ;(db/mass-insert! (import-csv "ta.csv"))
 
